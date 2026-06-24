abfd1d80a79510c2150ab28cbc88ea88fbf32a7a
Aaryan Pashine <pashineaaryan@gmail.com>
2026-06-24 18:19:23 -0400

    ok

--- netlify/auth.js
+++ netlify/auth.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = "7LdAwx2v-bKI_h8wbP0L2O-eFja3VujgHIHPk5S8-wY";
 const CLIENT_SECRET = "1303c7003860c62005a0a6c93d323555b9df40e6b7820f50b3a68e7426a15aa0";
-const REDIRECT_URI = "https://pop-os.tail0ff7f6.ts.net/";
+const REDIRECT_URI = "https://kaleidoscopic-druid-9d3ee7.netlify.app/.netlify/functions/auth";
 
 const AUTHORIZATION_URL = "https://www.are.na/oauth/authorize";
 const TOKEN_URL = "https://api.are.na/v3/oauth/token";
